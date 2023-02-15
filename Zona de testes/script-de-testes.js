d915369154e96d3eb3333294f3ff9a89865875e2
Vinicius Souza <vinicipkt@live.com>
2023-02-15 17:51:23 -0300

    Jogos selecionáveis

--- Zona de testes/script-de-testes.js
+++ Zona de testes/script-de-testes.js
@@ -1,5 +1,121 @@
 const listaDeJogos = document.getElementById('text-area-id')
 
+
+
+/* Lista de jogos */
+
+ /*  MODELO BASE (APAGAR DEPOIS)
+
+  function XXXXX() {
+    const jogo = '(XXXXX, XXXXXXXX), ';
+    const conteudoAtual = listaDeJogos.innerHTML;
+    const XXXXX = document.getElementById('XXXXXX')
+  
+    if (conteudoAtual.indexOf(jogo) !== -1) {
+      listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
+
+      XXXXX.style.background = 'red'
+    } else {
+      listaDeJogos.innerHTML = conteudoAtual + jogo + '';
+      
+      XXXXX.style.background = 'green'
+    }
+  } 
+  */
+
 function SBLE5G() {
-    listaDeJogos.innerText += `(SBLE5G, A boy and his blob), `
-}
+    const jogo = '(SBLE5G, A boy and his blob), ';
+    const conteudoAtual = listaDeJogos.innerHTML;
+    const ABAHB = document.getElementById('SBLE5G')
+  
+    if (conteudoAtual.indexOf(jogo) !== -1) {
+      listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
+
+      ABAHB.style.background = 'red'
+    } else {
+      listaDeJogos.innerHTML = conteudoAtual + jogo + '';
+      
+      ABAHB.style.background = 'green'
+    }
+  }
+
+  function SDWP18() {
+    const jogo = '(SDWP18, A shadows Tale), ';
+    const conteudoAtual = listaDeJogos.innerHTML;
+    const AST = document.getElementById('SDWP18')
+  
+    if (conteudoAtual.indexOf(jogo) !== -1) {
+      listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
+
+      AST.style.background = 'red'
+    } else {
+      listaDeJogos.innerHTML = conteudoAtual + jogo + '';
+      
+      AST.style.background = 'green'
+    }
+  }
+
+  function R4LXUG() {
+    const jogo = '(R4LXUG, A. piggy Party), ';
+    const conteudoAtual = listaDeJogos.innerHTML;
+    const APP = document.getElementById('R4LXUG')
+  
+    if (conteudoAtual.indexOf(jogo) !== -1) {
+      listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
+
+      APP.style.background = 'red'
+    } else {
+      listaDeJogos.innerHTML = conteudoAtual + jogo + '';
+      
+      APP.style.background = 'green'
+    }
+  }
+
+  function R33E69() {
+    const jogo = '(R33E69, AC/DC LIVE: Rock Band Track Pack), ';
+    const conteudoAtual = listaDeJogos.innerHTML;
+    const ACDC = document.getElementById('R33E69')
+  
+    if (conteudoAtual.indexOf(jogo) !== -1) {
+      listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
+
+      ACDC.style.background = 'red'
+    } else {
+      listaDeJogos.innerHTML = conteudoAtual + jogo + '';
+      
+      ACDC.style.background = 'green'
+    }
+  } 
+
+  function R3HP6Z() {
+    const jogo = '(R3HP6Z, Agent Hugo Hula Holiday), ';
+    const conteudoAtual = listaDeJogos.innerHTML;
+    const AHHH = document.getElementById('R3HP6Z')
+  
+    if (conteudoAtual.indexOf(jogo) !== -1) {
+      listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
+
+      AHHH.style.background = 'red'
+    } else {
+      listaDeJogos.innerHTML = conteudoAtual + jogo + '';
+      
+      AHHH.style.background = 'green'
+    }
+  } 
+
+  function RRKP70() {
+    const jogo = '(RRKP70, Alone in the Dark 5), ';
+    const conteudoAtual = listaDeJogos.innerHTML;
+    const AITT = document.getElementById('RRKP70')
+  
+    if (conteudoAtual.indexOf(jogo) !== -1) {
+      listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
+
+      AITT.style.background = 'red'
+    } else {
+      listaDeJogos.innerHTML = conteudoAtual + jogo + '';
+      
+      AITT.style.background = 'green'
+    }
+  }
+  
