7e02a2a233bdba0f0eceb793d5274a519e29ca90
Vinicius Souza <vinicipkt@live.com>
2023-02-23 12:32:00 -0300

    permitido remover jogos com HD cheio

--- Zona de testes/script-de-testes.js
+++ Zona de testes/script-de-testes.js
@@ -40,10 +40,6 @@ function selecionouHd2tb() {
     const jogo = '(XXXXX, XXXXXXXXXX), ';
     const conteudoAtual = listaDeJogos.innerHTML;
     const XXXXX = document.getElementById('XXXXX')
-
-    if (capacidadeAtual + XXXXX > capacidadeMaxima) {
-      alert('Você excedeu o tamanho máximo!')
-    }else {
   
     if (conteudoAtual.indexOf(jogo) !== -1) {
       listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
@@ -51,12 +47,15 @@ function selecionouHd2tb() {
       capacidadeAtual = capacidadeAtual - XXXXX
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
     } else {
+      if (capacidadeAtual + XXXXX > capacidadeMaxima) {
+      alert('Você excedeu o tamanho máximo!')
+      }else {
       listaDeJogos.innerHTML = conteudoAtual + jogo + '';
       XXXXX.style.background = 'green'
       capacidadeAtual = capacidadeAtual + XXXXX
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
+      }
     }
-  }
   } else {
     res.innerHTML = 'Selecione um HD'
   }
@@ -68,10 +67,6 @@ function SBLE5G() {
     const jogo = '(SBLE5G, A boy and his blob), ';
     const conteudoAtual = listaDeJogos.innerHTML;
     const ABAHB = document.getElementById('SBLE5G')
-
-    if (capacidadeAtual + 1268 > capacidadeMaxima) {
-      alert('Você excedeu o tamanho máximo!')
-    }else {
   
     if (conteudoAtual.indexOf(jogo) !== -1) {
       listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
@@ -79,12 +74,15 @@ function SBLE5G() {
       capacidadeAtual = capacidadeAtual - 1268
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
     } else {
+      if (capacidadeAtual + 1268 > capacidadeMaxima) {
+        alert('Você excedeu o tamanho máximo!')
+      }else {
       listaDeJogos.innerHTML = conteudoAtual + jogo + '';
       ABAHB.style.background = 'green'
       capacidadeAtual = capacidadeAtual + 1268
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
+      }
     }
-  }
   } else {
     res.innerHTML = 'Selecione um HD'
   }
@@ -95,10 +93,6 @@ function SDWP18() {
     const jogo = '(SDWP18, A shadows Tale), ';
     const conteudoAtual = listaDeJogos.innerHTML;
     const AST = document.getElementById('SDWP18')
-
-    if (capacidadeAtual + 1276 > capacidadeMaxima) {
-      alert('Você excedeu o tamanho máximo!')
-    }else {
   
     if (conteudoAtual.indexOf(jogo) !== -1) {
       listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
@@ -106,12 +100,15 @@ function SDWP18() {
       capacidadeAtual = capacidadeAtual - 1276
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
     } else {
+      if (capacidadeAtual + 1276 > capacidadeMaxima) {
+        alert('Você excedeu o tamanho máximo!')
+      }else {
       listaDeJogos.innerHTML = conteudoAtual + jogo + '';
       AST.style.background = 'green'
       capacidadeAtual = capacidadeAtual + 1276
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
+      }
     }
-  }
   } else {
     res.innerHTML = 'Selecione um HD'
   }
@@ -122,10 +119,6 @@ function R4LXUG() {
     const jogo = '(R4LXUG, A. piggy Party), ';
     const conteudoAtual = listaDeJogos.innerHTML;
     const APP = document.getElementById('R4LXUG')
-
-    if (capacidadeAtual + 180 > capacidadeMaxima) {
-      alert('Você excedeu o tamanho máximo!')
-    }else {
   
     if (conteudoAtual.indexOf(jogo) !== -1) {
       listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
@@ -133,12 +126,15 @@ function R4LXUG() {
       capacidadeAtual = capacidadeAtual - 180
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
     } else {
+      if (capacidadeAtual + 180 > capacidadeMaxima) {
+        alert('Você excedeu o tamanho máximo!')
+      }else {
       listaDeJogos.innerHTML = conteudoAtual + jogo + '';
       APP.style.background = 'green'
       capacidadeAtual = capacidadeAtual + 180
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
+      }
     }
-  }
   } else {
     res.innerHTML = 'Selecione um HD'
   }
@@ -149,10 +145,6 @@ function R33E69() {
     const jogo = '(R33E69, AC/DC LIVE: Rock Band Track Pack), ';
     const conteudoAtual = listaDeJogos.innerHTML;
     const ACDC = document.getElementById('R33E69')
-
-    if (capacidadeAtual + 2690 > capacidadeMaxima) {
-      alert('Você excedeu o tamanho máximo!')
-    }else {
   
     if (conteudoAtual.indexOf(jogo) !== -1) {
       listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
@@ -160,12 +152,15 @@ function R33E69() {
       capacidadeAtual = capacidadeAtual - 2690
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
     } else {
+      if (capacidadeAtual + 2690 > capacidadeMaxima) {
+        alert('Você excedeu o tamanho máximo!')
+      }else {
       listaDeJogos.innerHTML = conteudoAtual + jogo + '';
       ACDC.style.background = 'green'
       capacidadeAtual = capacidadeAtual + 2690
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
+      }
     }
-  }
   } else {
     res.innerHTML = 'Selecione um HD'
   }
@@ -176,10 +171,6 @@ function R3HP6Z() {
     const jogo = '(R3HP6Z, Agent Hugo Hula Holiday), ';
     const conteudoAtual = listaDeJogos.innerHTML;
     const AHHH = document.getElementById('R3HP6Z')
-
-    if (capacidadeAtual + 436 > capacidadeMaxima) {
-      alert('Você excedeu o tamanho máximo!')
-    }else {
   
     if (conteudoAtual.indexOf(jogo) !== -1) {
       listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
@@ -187,12 +178,15 @@ function R3HP6Z() {
       capacidadeAtual = capacidadeAtual - 436
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
     } else {
+      if (capacidadeAtual + 436 > capacidadeMaxima) {
+        alert('Você excedeu o tamanho máximo!')
+      }else {
       listaDeJogos.innerHTML = conteudoAtual + jogo + '';
       AHHH.style.background = 'green'
       capacidadeAtual = capacidadeAtual + 436
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
+      }
     }
-  }
   } else {
     res.innerHTML = 'Selecione um HD'
   }
@@ -203,10 +197,6 @@ function RRKP70() {
     const jogo = '(RRKP70, Alone in the Dark 5), ';
     const conteudoAtual = listaDeJogos.innerHTML;
     const AITD = document.getElementById('RRKP70')
-
-    if (capacidadeAtual + 4194 > capacidadeMaxima) {
-      alert('Você excedeu o tamanho máximo!')
-    }else {
   
     if (conteudoAtual.indexOf(jogo) !== -1) {
       listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
@@ -214,12 +204,15 @@ function RRKP70() {
       capacidadeAtual = capacidadeAtual - 4194
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
     } else {
+      if (capacidadeAtual + 4194 > capacidadeMaxima) {
+        alert('Você excedeu o tamanho máximo!')
+      }else {
       listaDeJogos.innerHTML = conteudoAtual + jogo + '';
       AITD.style.background = 'green'
       capacidadeAtual = capacidadeAtual + 4194
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
+      }
     }
-  }
   } else {
     res.innerHTML = 'Selecione um HD'
   }
@@ -230,10 +223,6 @@ function SALP4Q() {
     const jogo = '(SALP4Q, Alice In Wonderland ), ';
     const conteudoAtual = listaDeJogos.innerHTML;
     const AIW = document.getElementById('SALP4Q')
-
-    if (capacidadeAtual + 3896 > capacidadeMaxima) {
-      alert('Você excedeu o tamanho máximo!')
-    }else {
   
     if (conteudoAtual.indexOf(jogo) !== -1) {
       listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
@@ -241,12 +230,15 @@ function SALP4Q() {
       capacidadeAtual = capacidadeAtual - 3896
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
     } else {
+      if (capacidadeAtual + 3896 > capacidadeMaxima) {
+        alert('Você excedeu o tamanho máximo!')
+      }else {
       listaDeJogos.innerHTML = conteudoAtual + jogo + '';
       AIW.style.background = 'green'
       capacidadeAtual = capacidadeAtual + 3896
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
+      }
     }
-  }
   } else {
     res.innerHTML = 'Selecione um HD'
   }
@@ -258,22 +250,21 @@ function SFSPGT() {
     const conteudoAtual = listaDeJogos.innerHTML;
     const ARH = document.getElementById('SFSPGT')
 
-    if (capacidadeAtual + 430 > capacidadeMaxima) {
-      alert('Você excedeu o tamanho máximo!')
-    }else {
-  
     if (conteudoAtual.indexOf(jogo) !== -1) {
       listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
       ARH.style.background = 'red'
       capacidadeAtual = capacidadeAtual - 430
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
     } else {
+      if (capacidadeAtual + 430 > capacidadeMaxima) {
+        alert('Você excedeu o tamanho máximo!')
+      }else {
       listaDeJogos.innerHTML = conteudoAtual + jogo + '';
       ARH.style.background = 'green'
       capacidadeAtual = capacidadeAtual + 430
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
+      }
     }
-  }
   } else {
     res.innerHTML = 'Selecione um HD'
   }
@@ -281,26 +272,26 @@ function SFSPGT() {
 
 function RVBERS() {
   if(hd500.checked || hd1tb.checked || hd2tb.checked) {
-    const jogo = '(RVBERS, Alvin & The Chipmunks), ';
+    const jogo = '(RVBERS, Alvin and The Chipmunks), ';
     const conteudoAtual = listaDeJogos.innerHTML;
     const ATC = document.getElementById('RVBERS')
 
-    if (capacidadeAtual + 2156 > capacidadeMaxima) {
-      alert('Você excedeu o tamanho máximo!')
-    }else {
-  
     if (conteudoAtual.indexOf(jogo) !== -1) {
       listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
       ATC.style.background = 'red'
       capacidadeAtual = capacidadeAtual - 2156
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
     } else {
+      if (capacidadeAtual + 2156 > capacidadeMaxima) {
+        alert('Você excedeu o tamanho máximo!')
+      }else {
       listaDeJogos.innerHTML = conteudoAtual + jogo + '';
       ATC.style.background = 'green'
       capacidadeAtual = capacidadeAtual + 2156
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
+      }
     }
-  }
+  
   } else {
     res.innerHTML = 'Selecione um HD'
   }
@@ -312,22 +303,22 @@ function SAVE5G() {
     const conteudoAtual = listaDeJogos.innerHTML;
     const ATCTS = document.getElementById('SAVE5G')
 
-    if (capacidadeAtual + 652 > capacidadeMaxima) {
-      alert('Você excedeu o tamanho máximo!')
-    }else {
-  
     if (conteudoAtual.indexOf(jogo) !== -1) {
       listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
       ATCTS.style.background = 'red'
       capacidadeAtual = capacidadeAtual - 652
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
     } else {
+      if (capacidadeAtual + 652 > capacidadeMaxima) {
+        alert('Você excedeu o tamanho máximo!')
+      }else {
       listaDeJogos.innerHTML = conteudoAtual + jogo + '';
       ATCTS.style.background = 'green'
       capacidadeAtual = capacidadeAtual + 652
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
+      }
     }
-  }
+  
   } else {
     res.innerHTML = 'Selecione um HD'
   }
@@ -339,19 +330,19 @@ function S7DE52() {
     const conteudoAtual = listaDeJogos.innerHTML;
     const ABSW = document.getElementById('S7DE52')
 
-    if (capacidadeAtual + 904 > capacidadeMaxima) {
+    if (capacidadeAtual + 490000 > capacidadeMaxima) {
       alert('Você excedeu o tamanho máximo!')
     }else {
   
     if (conteudoAtual.indexOf(jogo) !== -1) {
       listaDeJogos.innerHTML = conteudoAtual.replace(jogo, '');
       ABSW.style.background = 'red'
-      capacidadeAtual = capacidadeAtual - 904
+      capacidadeAtual = capacidadeAtual - 490000
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
     } else {
       listaDeJogos.innerHTML = conteudoAtual + jogo + '';
       ABSW.style.background = 'green'
-      capacidadeAtual = capacidadeAtual + 904
+      capacidadeAtual = capacidadeAtual + 490000
       res.innerHTML = `${capacidadeAtual} / ${capacidadeMaxima}`
     }
   }
